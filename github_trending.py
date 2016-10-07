782f07ccac6ac232ee349c7e4f5833edc1ad57d3
aleksei-g <lexa_prorock@mail.ru>
2016-10-07 18:30:17 +0300

    Исправлен requests.get

--- github_trending.py
+++ github_trending.py
@@ -11,14 +11,10 @@ def get_trending_repositories():
     created_prior_to_date = date.today() -\
         timedelta(days=created_repository_days_ago)
     url = 'https://api.github.com/search/repositories'
-    payload = {'q': '+created:>=%s' % created_prior_to_date,
+    payload = {'q': 'created:>=%s' % created_prior_to_date,
                'sort': 'stars',
                'order': 'desc'}
-    # если не переводить payload в строку запрос кодируется (напр: "+" в "%2B")
-    # и в результате не получаем нужного ответа
-    payload_str = "&".join("%s=%s" % (key, val)
-                           for key, val in payload.items())
-    response = requests.get(url, params=payload_str)
+    response = requests.get(url, params=payload)
     return response.json()['items'][0:TOP_SIZE]
 
 
